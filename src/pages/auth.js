d0c49c26fc9c79ccfc3f4d8b70282dbfe8da1d3f
Kunal G <kunalgursal2923@gmail.com>
2023-03-16 09:58:31 +0530

    pages created

---
+++ src/pages/auth.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "./login";
+import Register from "./register";
+
+
+export const Auth = () => {
+  return (
+    <div className="auth">
+      <Login />
+      <Register/>
+    </div>
+  );
+};
+
+
+
